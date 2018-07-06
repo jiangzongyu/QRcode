1e4143054323accdd41b4b9c052fba932460d684
jiangzongyu <jiangzongyu@100tal.com>
2018-07-06 11:50:44 +0800

    组件封装QRcode

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-tooltip placement="top">
+      <img slot="content" :src="src" class="QRcodeSide">
+      <span v-text="name"></span>
+    </el-tooltip>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        aaa: '123',
+        content: '1231231'
+      }
+    },
+    props: {
+      src: {
+        default: ''
+      },
+      name: {
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style>
+  .QRcodeSide {
+    width: 150px;
+    height: 150px;
+  }
+</style>
---
+++ index1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <QRcode :src = src :name="teacherName">
+    </QRcode>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        teacherName: 'jiang',
+        src: 'https://box.bdimg.com/static/fisp_static/common/img/searchbox/logo_news_276_88_1f9876a.png',
+    components: {
+      QRcode
+    },
+  }
+</script>
